69ef01f24abe7c39e7ab3c5a54f9ecb8dc9f8666
JoseRG03 <114616625+JoseRG03@users.noreply.github.com>
2025-01-03 23:19:37 -0400

    feat: Featured items card mock created

--- lib/pages/app/store/home-page.dart
+++ lib/pages/app/store/home-page.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -5,39 +6,143 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-        padding: const EdgeInsets.all(8.0),
-          child: Column(
+    return Column(
+      children: [
+        Row(
+          children: [
+            Expanded(
+                child: TextField(
+              decoration: InputDecoration(hintText: "Buscar..."),
+            )),
+            IconButton(onPressed: () {}, icon: Icon(Icons.filter_alt))
+          ],
+        ),
+        SizedBox(height: 10),
+        Expanded(
+          child: ListView(
             children: [
-              Row(
-                children: [
-                  Expanded(child: TextField(decoration: InputDecoration(hintText: "Buscar..."),)),
-                  IconButton(onPressed: () {}, icon: Icon(Icons.filter_alt))
-                ],
+              Text('Destacados:'),
+              SizedBox(
+                height: 400,
+                child: TopItemsCarousel(),
               ),
-              SizedBox(height: 10),
-              Expanded(
-                child: ListView(
-                  children: [
-                    Text('Destacados:'),
-                    SizedBox(
-                      height: 400,
-                      child: CarouselView(
-                        itemExtent: double.infinity,
-                        scrollDirection: Axis.horizontal,
-                        children: List<Widget>.generate(10, (int index) {
-                          return Card(child: Center(child: Text('Item $index')));
-                        }),
-                      ),
-                    ),
-                    const SizedBox(height: 20),
-                    Text('Todos los Productos:'),
-                    ProductList(),
-                  ],
-                ),
-              ),
+              const SizedBox(height: 20),
+              Text('Todos los Productos:'),
+              ProductList(),
             ],
           ),
+        ),
+      ],
+    );
+  }
+}
+
+class TopItemsCarousel extends StatelessWidget {
+  const TopItemsCarousel({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return CarouselSlider(
+      options: CarouselOptions(
+        height: 400,
+      ),
+      items: List<Widget>.generate(10, (int index) {
+        return FeaturedItemCard();
+      }),
+    );
+  }
+}
+
+class FeaturedItemCard extends StatelessWidget {
+  const FeaturedItemCard({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(20),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(25),
+        color: Colors.white,
+        boxShadow: const [
+          BoxShadow(
+              color: Colors.grey,
+              offset: Offset(2.5, 2.5),
+              blurRadius: 10.0,
+              spreadRadius: 3.0)
+        ],
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            height: 200,
+            width: double.infinity,
+            child: Stack(children: [
+              Positioned(
+                child: Container(
+                  child: Padding(
+                    padding: const EdgeInsets.all(4.0),
+                    child: Text('\$499.99 DOP'),
+                  ),
+                  decoration: BoxDecoration(
+                      color: Colors.yellow,
+                      borderRadius: BorderRadius.circular(5)),
+                ),
+                top: 20,
+                right: 20,
+              )
+            ]),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(25),
+              image: const DecorationImage(
+                  image: AssetImage(
+                      'assets/sample-furniture-images/work_chair.jpg'),
+                  fit: BoxFit.cover),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.only(
+                  top: 16.0, bottom: 8.0, left: 16.0, right: 16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Card Title",
+                    style: TextStyle(
+                        fontSize: 18, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(height: 5),
+                  Text(
+                    'Greyhound divisively hello coldly wonder marginally far upon excluding. Greyhound divisively hello coldly wonder marginally far upon excluding.',
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  Row(
+                    children: [
+                      MaterialButton(
+                          onPressed: () {
+                            print('Ver detalles');
+                          },
+                          child: Text('Ver Detalles')),
+                      MaterialButton(
+                          onPressed: () {
+                            print('Carrito');
+                          },
+                          child: Text('Agregar al Carrito')),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
