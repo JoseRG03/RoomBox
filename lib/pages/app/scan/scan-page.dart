bb0568cd8e7b561c4c8dbab6d32a8dbc50e0a756
JoseRG03 <114616625+JoseRG03@users.noreply.github.com>
2025-01-12 23:25:08 -0400

    Camera functionality added

--- lib/pages/app/scan/scan-page.dart
+++ lib/pages/app/scan/scan-page.dart
@@ -1,10 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:mobile_scanner/mobile_scanner.dart';
+
+import '../store/item-description.dart';
 
 class ScanPage extends StatelessWidget {
   const ScanPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text('Scan'),);
+    return MobileScanner(
+      controller: MobileScannerController(detectionSpeed: DetectionSpeed.noDuplicates),
+      onDetect: (capture) {
+        final List<Barcode> barcodes = capture.barcodes;
+
+        for (final barcode in barcodes) {
+          if (barcode.rawValue != null) {
+            Navigator.of(context).push(MaterialPageRoute(
+                builder: (context) => ItemDescription(itemID: barcode.rawValue ?? '')));
+          }
+
+        }
+
+      },
+    );
   }
 }
